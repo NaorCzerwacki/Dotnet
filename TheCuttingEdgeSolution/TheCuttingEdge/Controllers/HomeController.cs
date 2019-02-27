9c741d42fe7147a70216c0303c4069257abc6664
Naor Czerwacki <servebaki@hotmail.com>
2019-02-27 16:44:47 +0200

    updates

--- TheCuttingEdgeSolution/TheCuttingEdge/Controllers/HomeController.cs
+++ TheCuttingEdgeSolution/TheCuttingEdge/Controllers/HomeController.cs
@@ -35,7 +35,12 @@ namespace TheCuttingEdge.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult MyRecipes()
+        {
+            return View();
+        }        
+
+       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
